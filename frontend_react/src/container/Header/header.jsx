74b89a1a39a2efca9ed6c450e0d2fe3cc0e8061e
Bimasakti <pamungkasbimasakti99@gmail.com>
2022-04-30 00:19:50 +0700

    Setting up container folder

---
+++ frontend_react/src/container/Header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Header.scss'
+
+const header = () => {
+  return (
+    <div>header</div>
+  )
+}
+
+export default header
---
+++ frontend_react/src/container/Testimonials/testimonials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Testimonials.scss'
+
+const testimonials = () => {
+  return (
+    <div>
+      testimonials
+    </div>
+  )
+}
+
+export default testimonials
